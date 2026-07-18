e64c3c782334922ac79d03cee7911041cfb5ab76
krishnamurthyrukeswar <krishnamurthyrukeswar@outlook.com>
2026-07-18 17:23:40 +0530

    Fix repositories for F-Droid

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,20 +1,17 @@
 pluginManagement {
     repositories {
-        maven { url = uri("https://dl.google.com/dl/android/maven2/") }
-        maven { url = uri("https://repo1.maven.org/maven2/") }
-        maven { url = uri("https://plugins.gradle.org/m2/") }
-        maven { url = uri("https://repo.gradle.org/gradle/libs-releases") }
+        google()
+        mavenCentral()
         gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven { url = uri("https://dl.google.com/dl/android/maven2/") }
-        maven { url = uri("https://repo1.maven.org/maven2/") }
+        google()
+        mavenCentral()
         maven { url = uri("https://jitpack.io") }
     }
 }
-
 rootProject.name = "LexiRead"
 include(":app")
